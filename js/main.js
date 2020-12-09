782cc4ba82be280cef64872a6cfbaa51cc5f6fbb
Elad Tal <eladtal14@gmail.com>
2020-12-09 17:22:41 +0200

    writing on top of the canvas works

--- js/main.js
+++ js/main.js
@@ -1,41 +1,27 @@
 'use strict'
 var gCanvas;
 var gCtx;
+var gImg
 
 function init() {
   gCanvas = document.querySelector('#my-canvas');
   gCtx = gCanvas.getContext('2d');
+  onSetFilter()
+
 }
 
-function drawImg(id) {
-  openWorkSpace()
-  var text = document.querySelector('.text').value;
-  var imgFromBack = getImg(id);
-  var img = new Image();
-  img.src = imgFromBack.url;
-  console.log(img);
-  img.onload = () => {
-    gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
-    drawText(text, 100, 100);
-  }
-}
 
-function drawText(text, x = 100, y = 100) {
-  gCtx.strokeStyle = 'red';
-  gCtx.font = 'italic small-caps 900 40px serif';
-  gCtx.textAlign = 'center';
-  gCtx.fillText(text, x, y);
-  gCtx.strokeText(text, x, y);
-}
 
-function openGallery() {
-  var workSpace = document.querySelector('.working-space');
-  workSpace.hidden = true;
-  var gallery = document.querySelector('.gallery');
-  console.log(gallery);
-  gallery.hidden = false;
-  renderPictures()
-}
+// function drawText(text, x = 100, y = 100) {
+//   if (!text) updateTxtMeme('')
+//   gCtx.strokeStyle = 'red';
+//   gCtx.font = 'small-caps 700 40px impact';
+//   gCtx.textAlign = 'center';
+//   gCtx.fillText(text, x, y);
+//   gCtx.strokeText(text, x, y);
+// }
+
+
 
 function onSetFilter(filterBy) {
   setFilter(filterBy);
@@ -51,10 +37,103 @@ function renderPictures() {
   gallery.innerHTML = strHTMLs.join('');
 }
 
+function openGallery() {
+  var workSpace = document.querySelector('.working-space');
+  workSpace.hidden = true;
+  var gallery = document.querySelector('.gallery');
+  gallery.hidden = false;
+  renderPictures()
+}
+
 function openWorkSpace() {
   var workSpace = document.querySelector('.working-space');
   workSpace.hidden = false;
   var gallery = document.querySelector('.gallery');
-  console.log(gallery);
   gallery.hidden = true;
+}
+
+
+function drawImg(id) {
+  openWorkSpace()
+  var text = document.querySelector('.text');
+  text.value = ''
+  var imgFromBack = getImg(id);
+  var img = new Image();
+  img.src = imgFromBack.url;
+  var meme = getMeme();
+  gImg = img
+  // console.log(img);
+  gImg.onload = () => {
+    drawMeme()
+
+    // gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
+    // drawText(meme.lines[0].txt, 250, 100);
+    // drawText(meme.lines[1].txt, 250, 400);
+  }
+
+}
+
+function addEventsToInput() {
+  var topText = document.querySelector('.text')
+  topText.addEventListener('keydown', drawMeme)
+  topText.addEventListener('keyup', drawMeme)
+  topText.addEventListener('change', drawMeme)
+}
+
+function drawMeme() {
+  addEventsToInput()
+  gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
+
+  gCtx.drawImage(gImg, 0, 0, gCanvas.width, gCanvas.height);
+
+  gCtx.lineWidth = 4;
+  gCtx.font = '40px impact';
+  gCtx.strokeStyle = 'black';
+  gCtx.fillStyle = 'white';
+  gCtx.textAlign = 'center';
+  gCtx.textBaseline = 'top';
+
+  var text = document.querySelector('.text').value;
+  text = text.toUpperCase();
+  var x = gCanvas.width / 2;
+  var y = 0;
+  wrapText(gCtx, text, x, y, 300, 28, false);
+  // ctx.textBaseline = 'bottom';
+  // var text2 = document.getElementById('bottom-text').value;
+  // text2 = text2.toUpperCase();
+  // y = memeSize;
+
+}
+
+function wrapText(context, text, x, y, maxWidth, lineHeight, fromBottom) {
+
+  var pushMethod = (fromBottom) ? 'unshift' : 'push';
+
+  lineHeight = (fromBottom) ? -lineHeight : lineHeight;
+
+  var lines = [];
+  var y = y;
+  var line = '';
+  var words = text.split(' ');
+
+  for (var n = 0; n < words.length; n++) {
+    var testLine = line + ' ' + words[n];
+    var metrics = context.measureText(testLine);
+    var testWidth = metrics.width;
+
+    if (testWidth > maxWidth) {
+      lines[pushMethod](line);
+      line = words[n] + ' ';
+    } else {
+      line = testLine;
+    }
+  }
+  lines[pushMethod](line);
+
+  for (var k in lines) {
+    context.strokeText(lines[k], x, y + lineHeight * k);
+    context.fillText(lines[k], x, y + lineHeight * k);
+  }
+
+
 }
--- js/meme-service.js
+++ js/meme-service.js
@@ -8,15 +8,55 @@ var gImgs = [{
   id: 1,
   url: 'images/1.jpg',
   keywords: ['happy', 'funny', 'man']
+}, {
+  id: 2,
+  url: 'images/2.jpg',
+  keywords: ['alone', 'sad']
+}, {
+  id: 3,
+  url: 'images/3.jpg',
+  keywords: ['happy', 'funny', 'man']
+}, {
+  id: 4,
+  url: 'images/4.jpg',
+  keywords: ['alone', 'sad']
+}, {
+  id: 5,
+  url: 'images/5.jpg',
+  keywords: ['happy', 'funny', 'man']
+}, {
+  id: 6,
+  url: 'images/6.jpg',
+  keywords: ['alone', 'sad']
+}, {
+  id: 7,
+  url: 'images/7.jpg',
+  keywords: ['happy', 'funny', 'man']
+}, {
+  id: 8,
+  url: 'images/8.jpg',
+  keywords: ['alone', 'sad']
 }];
 
 var gMeme = {
-  selectedImgId: 1,
+  selectedImgId: 0,
   selectedLineIdx: 0,
   lines: [{
-    txt: 'I never eat Falafel',
-  }]
+      txt: 'enter text'
+    },
+    {
+      txt: 'enter text',
+    }
+  ]
 };
+
+function updateTxtMeme(text) {
+  gMeme.lines[gMeme.selectedLineIdx].txt = text
+}
+
+function getMeme() {
+  return gMeme;
+}
 var gFilterBy;
 
 function getImg(idx) {
